8ff4072d089adf949f9c0f0c423ff916f9d860d3
olartem <89100800+olartem@users.noreply.github.com>
2023-05-05 13:27:36 +0300

    Add files via upload

--- Lab7/index.js
+++ Lab7/index.js
@@ -75,59 +75,59 @@ function triangle(value1, param1, value2, param2) {
 
   switch (true) {
     case (param1 === "leg" && param2 === "leg"):
-      console.log("1");
+      //console.log("1");
       c = Math.sqrt(a * a + b * b);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "leg" && param2 === "hypotenuse"):
-      console.log("2");
+      //console.log("2");
       b = Math.sqrt(c * c - a * a);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "hypotenuse" && param2 === "leg"):
-      console.log("3");
+      //console.log("3");
       a = Math.sqrt(c * c - b * b);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "leg" && param2 === "adjacent angle"):
-      console.log("4");
+      //console.log("4");
       c = a / Math.cos(alpha);
       b = Math.sqrt(c * c - a * a);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "adjacent angle" && param2 === "leg"):
-      console.log("5");
+      //console.log("5");
       c = b / Math.cos(alpha);
       a = Math.sqrt(c * c - b * b);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "leg" && param2 === "opposite angle"):
-      console.log("6");
+      //console.log("6");
       c = a / Math.sin(beta);
       b = Math.sqrt(c * c - a * a);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "opposite angle" && param2 === "leg"):
-      console.log("7");
+      //console.log("7");
       c = b / Math.sin(beta);
       a = Math.sqrt(c * c - b * b);
       alpha = Math.asin(a / c);
       beta = Math.asin(b / c);
       break;
     case (param1 === "angle" && param2 === "hypotenuse"):
-      console.log("8");
+      //console.log("8");
       a = c * Math.sin(alpha);
       b = c * Math.cos(alpha);
       beta = Math.PI / 2 - alpha;
       break;
     case (param1 === "hypotenuse" && param2 === "angle"):
-      console.log("9");
+      //console.log("9");
       a = c * Math.sin(alpha);
       b = c * Math.cos(alpha);
       beta = Math.PI / 2 - alpha;
